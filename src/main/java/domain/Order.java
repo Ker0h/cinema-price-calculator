5bda8744e3ef7d6f5fb9319acd4b744398914d78
Stefan <stefan131@outlook.com>
2020-01-29 16:29:24 +0100

    Add discount and premium fee for students

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -1,5 +1,7 @@
 package domain;
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 
 public class Order
@@ -27,22 +29,53 @@ public class Order
         tickets.add(ticket);
     }
 
-    public double calculatePrice()
-    {
+    public double calculatePrice() {
         int totalPrice = 0;
+        int totalAmount = 0;
+        int amountPremium = 0;
 
-        if(isStudentOrder) {
-            return totalPrice;
-        }
-
+        //Go through every ticket
         for (MovieTicket ticket : tickets) {
-            int price;
-
-            if(ticket.getMovieScreening()ticket.getMovieScreening().getDateAndTime().getDayOfWeek().getValue() <= 5)
-                totalPrice += tic;
+            //Count amount of premium tickets
+            if (ticket.isPremiumTicket()) {
+                amountPremium++;
+            }
+            //Add price to total of order
+            totalPrice += ticket.getPrice();
+            //Count amount of tickets (for 2nd free discount)
+            totalAmount++;
         }
+
+        //Check if it is a student order
+        if (isStudentOrder) {
+            //Calculate every second ticket as free
+            Pair newVals = secondTicketFree(totalAmount, amountPremium, totalPrice);
+            //Set the new price
+            totalPrice = (int) newVals.getKey();
+            amountPremium = (int) newVals.getValue();
+            //Add the fee for premium tickets
+            totalPrice += (amountPremium * 2);
+        }
+
 
         return 0;
+    }
+
+    private Pair<Integer, Integer> secondTicketFree(int totalAmount, int amountPremium, int totalPrice){
+
+        //If the total amount of tickets is uneven, remove one for the calculation (only 2nd is free)
+        //The discount is calculated as a 50% discount on the part of the price with even tickets
+        if ((totalAmount%2) != 0) {
+            totalPrice = ((((totalPrice / totalAmount) * (totalAmount - 1)) / 2)
+                            + ((totalPrice / totalAmount) * 1));
+        }
+
+        //If the amount of premium tickets is uneven, remove one for the calculation (only 2nd is free)
+        if ((amountPremium%2) != 0) {
+            amountPremium = ((amountPremium - 1) / 2) + 1;
+        }
+
+        return new Pair<Integer, Integer>(totalPrice, amountPremium);
     }
 
     public void export(TicketExportFormat exportFormat)
